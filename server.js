4b33fa5a1ec4376ae441e8162b1b1cc07abb6bf6
rashmisingh <rashmisingh@in.ibm.com>
2019-11-27 15:01:00 +0530

    changed

--- server.js
+++ server.js
@@ -74,29 +74,12 @@ var initDb = function(callback) {
 };
 
 app.get('/', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    var col = db.collection('counts');
-    // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
-      if (err) {
-        console.log('Error running count. Message:\n'+err);
-      }
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
-    });
-  } else {
-    res.render('index.html', { pageCountMessage : null});
-  }
+  res.setHeader("Access-Control-Allow-Credentials", "*");
+  res.render('sample3.html');
 });
 
 app.get('/pagecount', function (req, res) {
-  res.setHeader("Access-Control-Allow-Credentials", "*");
-  res.render('sample3.html');
+ res.send("Hello World");
 });
 
 // error handling
